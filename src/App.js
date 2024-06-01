77e19cc09ba8b793af78af9001d68636c1704115
Sasirekha1583 <128038468+Sasirekha1583@users.noreply.github.com>
2024-06-01 08:19:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import Nav from "./common/Nav";
+import Home from "./Pages/Home";
+import "./common/Commoncss.css";
+import About from "./Pages/About";
+
+import Education from "./Pages/Education";
+import Work from "./Pages/Work";
+import Contact from "./Pages/Contact";
+import Projects from "./Pages/Projects";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import React, { Route, Routes } from "react-router-dom";
+import PageNotFound from "./Pages/PageNotFound";
+
+import Learn from "./common/Learn";
+
+function App() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  return (
+    <div>
+      <Nav />
+      <Learn />
+      {/* <Home/>
+      <About />
+      <Services/>s
+      <Education/>
+      <Work/>
+      <Contact/> */}
+      <Routes>
+        <Route exact="true" path="/myportfolio" element={<Home />}></Route>
+        <Route path="/myportfolio" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        
+        <Route path="/work" element={<Work />}></Route>
+        <Route path="/education" element={<Education />}></Route>
+       
+        <Route path="/portfolio" element={<Projects />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
